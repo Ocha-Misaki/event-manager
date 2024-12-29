b25072b6938fc6dd0c7727109a223b8d6d1bb653
Ocha-Misaki <himawari.158fuji@gmail.com>
2024-12-29 18:03:14 +0900

    routesを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'site#index'
 
+  get 'events', to: 'site#index'
+  get 'events/new', to: 'site#index'
+  get 'events/:id', to: 'site#index'
+  get 'events/:id/edit', to: 'site#index'
+
   namespace :api do
     resources :events, only: [:index, :show, :create, :update, :destroy]
   end
